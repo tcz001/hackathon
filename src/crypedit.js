aa84982e1d30890223a229fce25099c5c5e77f95
Omar Diab <me@omardiab.com>
2018-06-26 22:38:20 -0700

    let -> const

--- src/crypedit.js
+++ src/crypedit.js
@@ -13,41 +13,25 @@ class Crypedit {
   }
 
   async crypeditContract() {
-    let exist;
-    try {
-      // let currentNetwork = this.web3.currentProvider.publicConfigStore._state.networkVersion
-      // if(currentNetwork === "1") { // mainnet
-      //   exist = await this.contract.at('0x0')
-      // } else if (currentNetwork === "3") {  // ropsten
-      //   exist = await this.contract.at('0x809b72f93776771128723ba54914920ba1341506')
-      // } else if (currentNetwork === "4") { // rinkeby
-      //   exist = await this.contract.at('0xe9880eedd4d44ec0b25f3a362af5b8dfd2754c62')
-      // } else {
-      exist = await this.contract.deployed();
-      // }
-    } catch (e) {
-      // ropsten by default
-      // exist = await this.contract.at('')
-    }
-    return exist;
+    return await this.contract.deployed();
   }
 
   putName = async value => {
-    let exist = await this.crypeditContract();
-    let owner = await this.myAddress();
+    const exist = await this.crypeditContract();
+    const owner = await this.myAddress();
 
     return await exist.PutName(value, { from: owner });
   };
 
   addressOf = async value => {
-    let exist = await this.crypeditContract();
-    let owner = await this.myAddress();
+    const exist = await this.crypeditContract();
+    const owner = await this.myAddress();
     return await exist.AddressOf(value, { from: owner });
   };
 
   nameOf = async value => {
-    let exist = await this.crypeditContract();
-    let owner = await this.myAddress();
+    const exist = await this.crypeditContract();
+    const owner = await this.myAddress();
     return await exist.NameOf(value, { from: owner });
   };
 
