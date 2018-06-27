0820d7f300a7b611ff502ce3a71f73f70c69e5ba
Omar Diab <me@omardiab.com>
2018-06-26 23:36:52 -0700

    format page more nicely

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Crypedit from "./crypedit";
 import Card, { CardHeader } from "material-ui/Card";
 import Button from "material-ui/Button";
 import TextField from "material-ui/TextField";
+import randomcolor from "randomcolor";
 
 const web3 = global.web3 && new Web3(global.web3.currentProvider);
 
@@ -22,184 +23,249 @@ class App extends Component {
     };
   }
 
-  async componentDidMount() {
+  _randomizeHeaderColor = () => {
+    this.setState({
+      headerColorInterval: setInterval(() => {
+        this.setState({ headerColor: randomcolor() });
+      }, 350)
+    });
+  };
+
+  componentDidMount() {
     if (!web3) {
       return;
     }
     this.crypedit = new Crypedit(web3);
+    this._randomizeHeaderColor();
+  }
+  componentWillUnmount() {
+    clearInterval(this.state.headerColorInterval);
   }
 
   render() {
     return (
-      <div className="container">
-        <div className="Row">
-          <Card className="PutName">
-            <CardHeader title="Put Name:" />
-            <TextField
-              className="input"
-              placeholder="宮水三葉 or 立花瀧"
-              fullWidth
-              onChange={e => this.setState({ putNameValue: e.target.value })}
-              value={this.putNameValue}
-            />
-            <Button
-              onClick={async () => {
+      <main>
+        <h1
+          style={
+            this.state.headerColor ? { color: this.state.headerColor } : {}
+          }
+        >
+          <marquee>🔑🗝🔥🦀🎤Cryptendorse!🔑🗝🔥🦀🎤</marquee>
+        </h1>
+        <h3>Tie a name to an address, and endorse the people you trust!</h3>
+        <div className="container">
+          <Card className="Card">
+            <form
+              onSubmit={async e => {
+                e.preventDefault();
                 try {
                   const { putNameValue } = this.state;
                   await this.crypedit.putName(putNameValue);
-                  alert(
-                    "You have successfully register your name " + putNameValue
-                  );
+                  this.setState({
+                    putNameResult: `You have successfully registered your name as ${putNameValue}`
+                  });
                 } catch (err) {
                   alert(err);
                 }
               }}
             >
-              put
-            </Button>
+              <CardHeader title="Put Name:" />
+              <TextField
+                className="input"
+                placeholder="宮水三葉 or 立花瀧"
+                fullWidth
+                onChange={e => this.setState({ putNameValue: e.target.value })}
+                value={this.putNameValue}
+              />
+              <Button type="submit">put</Button>
+              {this.state.putNameResult && (
+                <p className="Result">{this.state.putNameResult}</p>
+              )}
+            </form>
           </Card>
-        </div>
-        <div className="Row">
-          <Card className="AddressOf">
-            <CardHeader title="Find Address by Name" />
-            <TextField
-              className="input"
-              placeholder="宮水三葉 or 立花瀧"
-              fullWidth
-              onChange={e =>
-                this.setState({ addressByNameValue: e.target.value })
-              }
-              value={this.state.addressByNameValue}
-            />
-            <Button
-              onClick={async () => {
+
+          <Card className="Card">
+            <form
+              onSubmit={async e => {
+                e.preventDefault();
                 try {
                   const { addressByNameValue } = this.state;
                   const addr = await this.crypedit.addressOf(
                     addressByNameValue
                   );
-                  alert("Address of " + addressByNameValue + " is " + addr);
+                  this.setState({
+                    addressByNameResult: `Address of account named ${addressByNameValue} is ${addr}`
+                  });
                 } catch (err) {
                   alert(err);
                 }
               }}
             >
-              find
-            </Button>
+              <CardHeader title="Find Address by Name" />
+              <TextField
+                className="input"
+                placeholder="宮水三葉 or 立花瀧"
+                fullWidth
+                onChange={e =>
+                  this.setState({ addressByNameValue: e.target.value })
+                }
+                value={this.state.addressByNameValue}
+              />
+              <Button type="submit">find</Button>
+              {this.state.addressByNameResult && (
+                <p className="Result">{this.state.addressByNameResult}</p>
+              )}
+            </form>
           </Card>
-        </div>
-        <div className="Row">
-          <Card className="NameOf">
-            <CardHeader title="Find Name by Address" />
-            <TextField
-              className="input"
-              placeholder="0x5c47e30dc7F82167De8865aac3914Ce927C15918"
-              fullWidth
-              onChange={e =>
-                this.setState({ nameByAddressValue: e.target.value })
-              }
-              value={this.state.nameByAddressValue}
-            />
-            <Button
-              onClick={async () => {
+
+          <Card className="Card">
+            <form
+              onSubmit={async e => {
+                e.preventDefault();
                 try {
                   const { nameByAddressValue } = this.state;
                   const name = await this.crypedit.nameOf(nameByAddressValue);
-                  alert("Name of " + nameByAddressValue + " is " + name);
+                  this.setState({
+                    nameByAddressResult: `Name of account with address "${nameByAddressValue}" is "${name}"`
+                  });
                 } catch (err) {
                   alert(err);
                 }
               }}
             >
-              find
-            </Button>
+              <CardHeader title="Find Name by Address" />
+              <TextField
+                className="input"
+                placeholder="0x5c47e30dc7F82167De8865aac3914Ce927C15918"
+                fullWidth
+                onChange={e =>
+                  this.setState({ nameByAddressValue: e.target.value })
+                }
+                value={this.state.nameByAddressValue}
+              />
+              <Button type="submit">find</Button>
+              {this.state.nameByAddressResult && (
+                <p className="Result">{this.state.nameByAddressResult}</p>
+              )}
+            </form>
           </Card>
-          <Card className="endorsements">
-            <CardHeader title="Find Endorsements for Name" />
-            <TextField
-              className="input"
-              placeholder="君の名は"
-              fullWidth
-              onChange={e =>
-                this.setState({ endorsementsByNameValue: e.target.value })
-              }
-              value={this.state.endorsementsByNameValue}
-            />
-            <Button
-              onClick={async () => {
+
+          <Card className="Card">
+            <form
+              onSubmit={async e => {
+                e.preventDefault();
                 try {
                   const { endorsementsByNameValue } = this.state;
                   const endorsements = await this.crypedit.endorsementsForName(
                     endorsementsByNameValue
                   );
-                  alert(
-                    "Endorsements of " +
-                      endorsementsByNameValue +
-                      " is " +
-                      JSON.stringify(endorsements)
-                  );
+                  this.setState({
+                    endorsementsForNameResult: {
+                      queried: endorsementsByNameValue,
+                      endorsements
+                    }
+                  });
                 } catch (err) {
                   alert(err);
                 }
               }}
             >
-              find
-            </Button>
+              <CardHeader title="Find Endorsements for Name" />
+              <TextField
+                className="input"
+                placeholder="君の名は"
+                fullWidth
+                onChange={e =>
+                  this.setState({ endorsementsByNameValue: e.target.value })
+                }
+                value={this.state.endorsementsByNameValue}
+              />
+              <Button type="submit">find</Button>
+              {this.state.endorsementsForNameResult &&
+                (this.state.endorsementsForNameResult.endorsements.length ===
+                0 ? (
+                  <p className="Result">
+                    Account with name "{
+                      this.state.endorsementsForNameResult.queried
+                    }" has no endorsements
+                  </p>
+                ) : (
+                  <div className="Result">
+                    <p>
+                      Endorsements for name "{
+                        this.state.endorsementsForNameResult.queried
+                      }" are:
+                    </p>
+                    <ul>
+                      {this.state.endorsementsForNameResult.endorsements.map(
+                        (e, idx) => <li key={idx}>{e}</li>
+                      )}
+                    </ul>
+                  </div>
+                ))}
+            </form>
           </Card>
-          <Card className="endorsements">
-            <CardHeader title="Find Endorsements for Address" />
-            <TextField
-              className="input"
-              placeholder="您的地址"
-              fullWidth
-              onChange={e =>
-                this.setState({ endorsementsByAddressValue: e.target.value })
-              }
-              value={this.state.endorsementsByAddressValue}
-            />
-            <Button
-              onClick={async () => {
+
+          <Card className="Card">
+            <form
+              onSubmit={async e => {
+                e.preventDefault();
                 try {
                   const { endorsementsByAddressValue } = this.state;
                   const endorsements = await this.crypedit.endorsementsForAddress(
                     endorsementsByAddressValue
                   );
-                  alert(
-                    "Endorsements of " +
-                      endorsementsByAddressValue +
-                      " is " +
-                      JSON.stringify(endorsements)
-                  );
+                  this.setState({
+                    endorsementsForAddressResult: {
+                      queried: endorsementsByAddressValue,
+                      endorsements
+                    }
+                  });
                 } catch (err) {
                   alert(err);
                 }
               }}
             >
-              find
-            </Button>
+              <CardHeader title="Find Endorsements for Address" />
+              <TextField
+                className="input"
+                placeholder="您的地址"
+                fullWidth
+                onChange={e =>
+                  this.setState({ endorsementsByAddressValue: e.target.value })
+                }
+                value={this.state.endorsementsByAddressValue}
+              />
+              <Button type="submit">find</Button>
+              {this.state.endorsementsForAddressResult &&
+                (this.state.endorsementsForAddressResult.endorsements.length ===
+                0 ? (
+                  <p className="Result">
+                    Account with address "{
+                      this.state.endorsementsForAddressResult.queried
+                    }" has no endorsements
+                  </p>
+                ) : (
+                  <div className="Result">
+                    <p>
+                      Endorsements for address "{
+                        this.state.endorsementsForAddressResult.queried
+                      }" are:
+                    </p>
+                    <ul>
+                      {this.state.endorsementsForAddressResult.endorsements.map(
+                        (e, idx) => <li key={idx}>{e}</li>
+                      )}
+                    </ul>
+                  </div>
+                ))}
+            </form>
           </Card>
-          <Card className="endorse">
-            <CardHeader title="Endorse" />
-            <TextField
-              className="input"
-              placeholder="您的地址"
-              fullWidth
-              onChange={e =>
-                this.setState({ endorseAddressValue: e.target.value })
-              }
-              value={this.state.endorseAddressValue}
-            />
-            <TextField
-              className="input"
-              placeholder="Comment"
-              fullWidth
-              onChange={e =>
-                this.setState({ endorseCommentValue: e.target.value })
-              }
-              value={this.state.endorseCommentValue}
-            />
-            <Button
-              onClick={async () => {
+
+          <Card className="Card">
+            <form
+              onSubmit={async e => {
+                e.preventDefault();
                 try {
                   const {
                     endorseAddressValue,
@@ -209,19 +275,49 @@ class App extends Component {
                     endorseAddressValue,
                     endorseCommentValue
                   );
-                  alert(
-                    `Successfully endorsed address "${endorseAddressValue}" with comment "${endorseCommentValue}"`
-                  );
+                  this.setState({
+                    endorseResult: {
+                      address: endorseAddressValue,
+                      comment: endorseCommentValue
+                    }
+                  });
+                  alert();
                 } catch (err) {
                   alert(err);
                 }
               }}
             >
-              endorse
-            </Button>
+              <CardHeader title="Endorse" />
+              <TextField
+                className="input"
+                placeholder="您的地址"
+                fullWidth
+                onChange={e =>
+                  this.setState({ endorseAddressValue: e.target.value })
+                }
+                value={this.state.endorseAddressValue}
+              />
+              <TextField
+                className="input"
+                placeholder="Comment"
+                fullWidth
+                onChange={e =>
+                  this.setState({ endorseCommentValue: e.target.value })
+                }
+                value={this.state.endorseCommentValue}
+              />
+              <Button type="submit">endorse</Button>
+              {this.state.endorseResult && (
+                <p className="Result">
+                  Successfully endorsed address "{
+                    this.state.endorseResult.address
+                  }" with comment "{this.state.endorseResult.comment}"
+                </p>
+              )}
+            </form>
           </Card>
         </div>
-      </div>
+      </main>
     );
   }
 }
